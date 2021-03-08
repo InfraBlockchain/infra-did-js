9fb24a5ce7f5624925fb489bfb0ca9d3ee6f34c5
Bezalel Lim <bezalel.dev@gmail.com>
2021-03-08 13:40:17 +0900

    signJWT, verifyJWT tests

--- src/__tests__/index-test.ts
+++ src/__tests__/index-test.ts
@@ -1,14 +1,48 @@
 import InfraDID from '../index'
+import { Resolver } from 'did-resolver'
+import { getResolver } from "infra-did-resolver"
 
 describe('InfraDID', () => {
 
-  const networkId = 'local'
-  const registryContract ='infradidregi'
-  const rpcEndpoint = 'http://127.0.0.1:8888'
+  // const networkId = 'local'
+  // const registryContract ='infradidregi'
+  // const rpcEndpoint = 'http://127.0.0.1:8888'
 
-  const txfeePayerAccount = 'infradidinit'
-  const txfeePayerPrivateKey = '5HwviX14H6M2g4qgF8DU1CSWtxZqx2c5bDZQJBmQmgzCTyEoJtU' // EOS6hiaAMKE7iHd7BgfoKJ63JCFNoser35hu3KNsjLEgo8TV4P4db
+  // const txfeePayerAccount = 'infradidinit'
+  // const txfeePayerPrivateKey = '5HwviX14H6M2g4qgF8DU1CSWtxZqx2c5bDZQJBmQmgzCTyEoJtU' // EOS6hiaAMKE7iHd7BgfoKJ63JCFNoser35hu3KNsjLEgo8TV4P4db
 
+  const networkId = 'vapptest1'
+  const registryContract = 'fmapkumrotfc'
+  const rpcEndpoint = 'https://api.testnet.eos.io'
+
+  const txfeePayerAccount = 'qwexfhmvvdci'
+  const txfeePayerPrivateKey = '5KV84hXSJvu3nfqb9b1raRMnzvULaHH6Fsaz4xBZG2QbfPwMg76' // EOS6hiaAMKE7iHd7BgfoKJ63JCFNoser35hu3KNsjLEgo8TV4P4db
+
+  const confDefaults = {
+    networkId,
+    registryContract,
+    rpcEndpoint,
+    // jwtSigner?: any,
+    txfeePayerAccount,
+    txfeePayerPrivateKey,
+  }
+
+  const infraDidResolverConfig = {
+    networks : [
+      {
+        networkId,
+        registryContract,
+        rpcEndpoint: 'https://api.testnet.eos.io'
+      }
+    ]
+  }
+
+  let infraDidResolver, didResolver
+
+  beforeAll(async () => {
+    infraDidResolver = getResolver(infraDidResolverConfig)
+    didResolver = new Resolver(infraDidResolver)
+  })
 
   describe('DID creation', () => {
     it('should create pubKey DID (secp256k1)', () => {
@@ -28,14 +62,9 @@ describe('InfraDID', () => {
       // console.log({pubKeyDID})
 
       const conf = {
-        did: 'did:infra:local:PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx', //pubKeyDID.did,
+        ...confDefaults,
+        did: `did:infra:${networkId}:PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx`, //pubKeyDID.did,
         didOwnerPrivateKey: 'PVT_K1_2anMa3Wq7rkQy7AyNdqMDmkycT6emGnP857zmZa13FyhkHY9JD', //pubKeyDID.privateKey,
-        networkId,
-        registryContract,
-        rpcEndpoint,
-        // jwtSigner?: any,
-        txfeePayerAccount,
-        txfeePayerPrivateKey,
       }
 
       const didApi = new InfraDID(conf)
@@ -49,14 +78,11 @@ describe('InfraDID', () => {
 
     it('should remove pubkey DID attribute',async () => {
       const conf = {
-        did: 'did:infra:local:PUB_K1_7pM9qiBuHWF6WqRSjPTMfVYKV5ZFRavK4PkUq4oFhqi9Z46mWc',
+        ...confDefaults,
+        did: `did:infra:${networkId}:PUB_K1_7pM9qiBuHWF6WqRSjPTMfVYKV5ZFRavK4PkUq4oFhqi9Z46mWc`,
         didOwnerPrivateKey: 'PVT_K1_2Gowe7JiuzsxifyjKoQ2XNzXe1FcTax6vsGhU58Kxt4LuLFDyQ',
-        networkId,
-        registryContract,
-        rpcEndpoint,
-        // jwtSigner?: any,
-        txfeePayerAccount,
-        txfeePayerPrivateKey,
+        // did: `did:infra:${networkId}:PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx`, //pubKeyDID.did,
+        // didOwnerPrivateKey: 'PVT_K1_2anMa3Wq7rkQy7AyNdqMDmkycT6emGnP857zmZa13FyhkHY9JD', //pubKeyDID.privateKey,
       }
 
       const didApi = new InfraDID(conf)
@@ -73,14 +99,9 @@ describe('InfraDID', () => {
       // didOwnerPrivateKey: 'PVT_K1_2Gowe7JiuzsxifyjKoQ2XNzXe1FcTax6vsGhU58Kxt4LuLFDyQ',
 
       const conf = {
-        did: 'did:infra:local:PUB_K1_7pM9qiBuHWF6WqRSjPTMfVYKV5ZFRavK4PkUq4oFhqi9Z46mWc',
+        ...confDefaults,
+        did: `did:infra:${networkId}:PUB_K1_7pM9qiBuHWF6WqRSjPTMfVYKV5ZFRavK4PkUq4oFhqi9Z46mWc`,
         didOwnerPrivateKey: 'PVT_K1_2Gowe7JiuzsxifyjKoQ2XNzXe1FcTax6vsGhU58Kxt4LuLFDyQ',
-        networkId,
-        registryContract,
-        rpcEndpoint,
-        // jwtSigner?: any,
-        txfeePayerAccount,
-        txfeePayerPrivateKey,
       }
 
       // pubKeyDID: {
@@ -98,7 +119,7 @@ describe('InfraDID', () => {
 
     it('should revoke pubkey DID',async () => {
       const conf = {
-        did: 'did:infra:local:PUB_K1_7pM9qiBuHWF6WqRSjPTMfVYKV5ZFRavK4PkUq4oFhqi9Z46mWc',
+        did: `did:infra:${networkId}:PUB_K1_7pM9qiBuHWF6WqRSjPTMfVYKV5ZFRavK4PkUq4oFhqi9Z46mWc`,
         // didOwnerPrivateKey: 'PVT_K1_2Gowe7JiuzsxifyjKoQ2XNzXe1FcTax6vsGhU58Kxt4LuLFDyQ',
         didOwnerPrivateKey: 'PVT_K1_2YiPos21thxcTSrYLafUrvnHHLUkZKVxTdUdysJGAfjAAbZqNe',
         networkId,
@@ -118,14 +139,9 @@ describe('InfraDID', () => {
 
     it('should clear pubkey DID chain db rows',async () => {
       const conf = {
-        did: 'did:infra:local:PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx',
+        ...confDefaults,
+        did: `did:infra:${networkId}:PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx`,
         didOwnerPrivateKey: 'PVT_K1_2anMa3Wq7rkQy7AyNdqMDmkycT6emGnP857zmZa13FyhkHY9JD',
-        networkId,
-        registryContract,
-        rpcEndpoint,
-        // jwtSigner?: any,
-        txfeePayerAccount,
-        txfeePayerPrivateKey,
       }
 
       const didApi = new InfraDID(conf)
@@ -141,9 +157,15 @@ describe('InfraDID', () => {
       // const pubKeyDID = InfraDID.createPubKeyDIDsecp256k1(networkId)
       // console.log({pubKeyDID})
 
+      // const account = 'diduser22222'
+      // const accountPrivateKey = 'PVT_K1_2uTgvsmdT7U12HNqLg1Y8UQtAtgoDew2erCkNuPvqDMzcUspCS'
+
+      const account = 'jghpykcpaoko'
+      const accountPrivateKey = '5Hucf4g3riLDHVWKbLLbnQU2cqC5oAXoP6XiPjYGc4qS1ASqq5T'
+
       const conf = {
-        did: 'did:infra:local:diduser22222',
-        didOwnerPrivateKey: 'PVT_K1_2uTgvsmdT7U12HNqLg1Y8UQtAtgoDew2erCkNuPvqDMzcUspCS',
+        did: `did:infra:${networkId}:${account}`,
+        didOwnerPrivateKey: accountPrivateKey,
         networkId,
         registryContract,
         rpcEndpoint,
@@ -159,4 +181,46 @@ describe('InfraDID', () => {
       expect(resSetAttr.transaction_id).toBeDefined()
     })
   })
+
+  describe('Sign and verify JWT', () => {
+    it('should sign and verify JWT (Verifiable Credential)',async () => {
+      const signerDidConf = {
+        ...confDefaults,
+        did: `did:infra:${networkId}:PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx`, //pubKeyDID.did,
+        didOwnerPrivateKey: 'PVT_K1_2anMa3Wq7rkQy7AyNdqMDmkycT6emGnP857zmZa13FyhkHY9JD', //pubKeyDID.privateKey,
+        // didOwnerPrivateKey: 'PVT_K1_2YiPos21thxcTSrYLafUrvnHHLUkZKVxTdUdysJGAfjAAbZqNe'
+      }
+
+      const verifierDidConf = {
+        ...confDefaults,
+        did: `did:infra:${networkId}:PUB_K1_6XpzkXC8amUN1AQccYcVpRMBajq8b3HHhYJVZ4uJQ7pW9TJvmr`, //pubKeyDID.did,
+        didOwnerPrivateKey: 'PVT_K1_22gQdGw4JPaHs3E9kkwKZ2mW9WR1YE6XnyDN6juTSaet7ecSGN', //pubKeyDID.privateKey,
+      }
+
+      const audienceDid = verifierDidConf.did
+
+      const signerDidApi = new InfraDID(signerDidConf)
+      const payload = {
+        aud: audienceDid,
+        claim1: 'claim1_value',
+        claim2: 'claim2_value'
+      }
+      const jwt = await signerDidApi.signJWT(payload)
+      console.log({jwt})
+
+      const verifierDidApi = new InfraDID(verifierDidConf)
+      const resVerify = await verifierDidApi.verifyJWT(jwt, didResolver, audienceDid)
+
+      console.dir(resVerify, { depth: null })
+      expect(resVerify.payload.aud === audienceDid).toBeTruthy()
+      expect(resVerify.payload.claim1 === payload.claim1).toBeTruthy()
+      expect(resVerify.payload.claim2 === payload.claim2).toBeTruthy()
+      expect(resVerify.payload.iss === signerDidConf.did).toBeTruthy()
+      expect(resVerify.issuer === signerDidConf.did).toBeTruthy()
+      expect(resVerify.jwt === jwt).toBeTruthy()
+
+    })
+  })
+
+
 })
